1c11febd4662d7ada768c23b3d7309126b39f148
Marco Andrei Kichalowsky <mandrei@arsnova.digital>
2024-12-21 19:37:09 -0300

    Nova base de dados.

--- esq_ufo.sql
+++ esq_ufo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Tempo de geração: 07/12/2024 às 22:21
+-- Tempo de geração: 21/12/2024 às 22:36
 -- Versão do servidor: 5.7.44
 -- Versão do PHP: 8.2.20
 
@@ -33,19 +33,29 @@ CREATE TABLE `cedentes` (
   `nacionalidade` varchar(32) NOT NULL,
   `estado_civil` char(1) NOT NULL,
   `profissao` varchar(32) NOT NULL,
-  `cpf` varchar(11) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
   `rg` varchar(11) NOT NULL,
-  `sexo` tinyint(1) NOT NULL COMMENT 'F=feminino; M=masculino; O=outro',
+  `sexo` char(1) NOT NULL COMMENT 'F=feminino; M=masculino; O=outro',
   `data_nasc` date NOT NULL,
   `endereco` varchar(200) NOT NULL,
   `cidade` varchar(64) NOT NULL,
   `estado` varchar(2) NOT NULL,
   `pais` varchar(64) NOT NULL,
   `email` varchar(128) NOT NULL,
-  `celular` varchar(14) NOT NULL,
-  `selfie_nomearq` varchar(512) NOT NULL,
-  `identidade_nomearq` varchar(512) NOT NULL
+  `celular` varchar(16) NOT NULL,
+  `selfie_nomearq` varchar(512) DEFAULT NULL,
+  `identidade_nomearq` varchar(512) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `responsavel` varchar(32) DEFAULT NULL,
+  `data_entrada` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Despejando dados para a tabela `cedentes`
+--
+
+INSERT INTO `cedentes` (`id`, `nome`, `nacionalidade`, `estado_civil`, `profissao`, `cpf`, `rg`, `sexo`, `data_nasc`, `endereco`, `cidade`, `estado`, `pais`, `email`, `celular`, `selfie_nomearq`, `identidade_nomearq`, `status`, `responsavel`, `data_entrada`) VALUES
+(24, 'Marco Andrei', 'brasileira', 'C', 'empresário', '673.253.350-91', '2044900617', 'M', '1972-09-07', 'Rua Uruguai 91 sala 516', 'Porto Alegre', 'RS', 'Brasil', 'mandrei@gmail.com', '(51) 9 9868-6436', 'Mad_Men_OG.pdf', '704478_175976452543680_1759067607_o.jpg', 0, NULL, '2024-12-21 22:34:51');
 
 -- --------------------------------------------------------
 
@@ -67,8 +77,15 @@ CREATE TABLE `obras` (
   `direcao_objeto` varchar(128) NOT NULL,
   `descricao_ocorrido` varchar(1024) NOT NULL,
   `testemunhas` varchar(512) NOT NULL,
-  `video_nomearq` varchar(256) NOT NULL
+  `video_nomearq` varchar(256) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Despejando dados para a tabela `obras`
+--
+
+INSERT INTO `obras` (`id`, `cedente_id`, `titulo`, `duracao`, `data_obra`, `ineditismo`, `data_gravacao`, `hora_gravacao`, `local_gravacao`, `cidade_gravacao`, `direcao_objeto`, `descricao_ocorrido`, `testemunhas`, `video_nomearq`) VALUES
+(13, 24, 'UFO', 20, '2010-10-01', 'I', '2010-10-01', '07:23:00', 'Rua das Flores', 'Matatu das Grota', 'Sul', 'Passou voando', 'Minha esposa', 'Snapinsta.app_video_474F6A58C562A54265F1DBBCDD578E91_video_dashinit.mp4');
 
 -- --------------------------------------------------------
 
@@ -101,12 +118,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `admin`) VALUES
-(1, 'Marco Andrei', 'mandrei@arsnova.digital', '67a34edf5f639998d674b841fc630683', 1),
-(2, 'Leo Sassen', 'leo@clipprodutora.com.br', '7477fed90dafec20dcf53b20ae9506a5', 0),
-(4, 'Leo Sassen', 'leo@clipprodutora.com.br', '7477fed90dafec20dcf53b20ae9506a5', 0),
-(5, 'Teste', 'teste@teste.com', '7b42e19968d8aa8db5d24ab5a0f35b0e', 0),
-(6, 'Leo Sassen', 'leo@clipprodutora.com.br', '7477fed90dafec20dcf53b20ae9506a5', 0),
-(7, 'Teste', 'teste@teste.com', '7b42e19968d8aa8db5d24ab5a0f35b0e', 0);
+(1, 'Marco Andrei', 'mandrei@arsnova.digital', '67a34edf5f639998d674b841fc630683', 1);
 
 --
 -- Índices para tabelas despejadas
@@ -144,19 +156,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `cedentes`
 --
 ALTER TABLE `cedentes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de tabela `obras`
 --
 ALTER TABLE `obras`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de tabela `obras_fotos`
 --
 ALTER TABLE `obras_fotos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
